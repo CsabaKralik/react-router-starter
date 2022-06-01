e63689e73ddb2a2842d76bb8038154c2f77c39de
Csaba Králik <kralikcsabi@gmail.com>
2022-06-01 10:39:18 +0200

    clear function

--- src/pages/CatFarm.jsx
+++ src/pages/CatFarm.jsx
@@ -1,10 +1,19 @@
-import CatGrid from '../components/CatGrid';
-
-const CatFarm = () => (
-  <div>
-    <h1>Cat Farm</h1>
-    <CatGrid />
-  </div>
-);
+import { useNavigate } from "react-router-dom";
+import CatGrid from "../components/CatGrid";
+const CatFarm = () => {
+  const clean = () => {
+    localStorage.removeItem("farm");
+    window.location.reload();
+  };
+  return (
+    <div>
+      <h1>Cat Farm</h1>
+      <button onClick={clean} className="clear-btn">
+        clear cats
+      </button>
+      <CatGrid />
+    </div>
+  );
+};
 
 export default CatFarm;
